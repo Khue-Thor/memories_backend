c82c832a794602572cbc0ddeabf33928339e72cf
khuephamy <khuephumy19@gmail.com>
2023-06-17 10:08:30 -0500

    cardItem Schema

--- models/cardItem.js
+++ models/cardItem.js
@@ -1,43 +1,39 @@
-const mongoose = require("mongoose");
-const bycrypt = require("bcrypt");
-const validator = require("validator");
-const { default: isEmail } = require("validator/lib/isEmail");
+const mongoose = require('mongoose');
 
-const userSchema = mongoose.Schema({
+const validator = require('validator');
+
+const clothingItem = mongoose.Schema({
   name: {
     type: String,
-    require: true,
-    minLength: 2,
-    maxLength: 30,
-    default: "Name",
+    required: true,
+    minlength: 2,
+    maxlength: 30,
   },
-  avatar: {
+  imageUrl: {
     type: String,
     required: true,
     validate: {
       validator(value) {
         return validator.isURL(value);
       },
-      message: "You must enter a valid URL",
-    },
-    default:
-      "https://images.unsplash.com/photo-1593085512500-5d55148d6f0d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8Y2FydG9vbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    validate: {
-      validator: (e) => isEmail(e),
-      message: "Email is invalid format",
+      message: 'You must enter a valid URL',
     },
   },
-  password: {
-    type: String,
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
     required: true,
-    select: false,
+  },
+  likes: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'user',
+    },
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now,
   },
 });
 
-
-module.exports = mongoose.model("user", userSchema);
+module.exports = mongoose.model('clothingItem', clothingItem);
